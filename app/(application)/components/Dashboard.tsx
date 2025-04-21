36e4275c3416ca354481e3d81ef37a9134a05fea
Malli7 <mallireddy2001@gmail.com>
2025-04-21 15:35:07 -0500

    updated delete function

--- app/(application)/components/Dashboard.tsx
+++ app/(application)/components/Dashboard.tsx
@@ -469,7 +469,7 @@ const DocumentList = ({
                     <Button
                       size="sm"
                       variant="outline"
-                      onClick={() => handleDeleteCoverLetter(key)}
+                      onClick={() => handleDeleteCoverLetter(value)}
                     >
                       <Trash className="h-4 w-4 mr-1" />
                       Delete
